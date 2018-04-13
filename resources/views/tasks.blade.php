6289a43a6831bb05c3c1fccb8c31c9c228304828
GuillaumeDron <35961722+GuillaumeDron@users.noreply.github.com>
2018-04-13 19:01:25 +0200

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -3,51 +3,7 @@
 @section('content')
     <div class="container">
         <div class="col-sm-offset-2 col-sm-8">
-            <header>
-                <img / id="logo" src="../images/logo.png" alt"Bug d'affichage :(">
-                <p class = "titrePage">
-                    Statistiques
-                </p>
-                <a href = "../php/connexion.php" class = "texteMenu">
-                    <div class = "boutonMenu" id = "boutonConnexion">
-                        Se connecter
-                    </div>
-                </a>
-            </header>
-            
-            <nav>
-                <a href = "../php/match.php" class = "texteMenu">
-                    <div class = "boutonMenu">
-                        Match
-                    </div>
-                </a>
-
-
-                <a href = "../php/actualite.php" class = "texteMenu">
-                    <div class = "boutonMenu">
-                        Actualité
-                    </div>
-                </a>
-
-                <a href = "../php/statistiques.php" class = "texteMenu">
-                    <div class = "boutonMenu">
-                        Statistiques des joueurs
-                    </div>
-                </a>
-
-
-                <a href = "../php/entrainement.php" class = "texteMenu">
-                    <div class = "boutonMenu">
-                        Entraînement
-                    </div>
-                </a>
-
-                <a href = "../php/reglages.php" class = "texteMenu">
-                    <div class = "boutonMenu">
-                        Réglages
-                    </div>
-                </a>
-            </nav>
+            Equipe
         </div>
     </div>
 @endsection
